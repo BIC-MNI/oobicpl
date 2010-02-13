77297ef4f2a42d99a6bc18a05c6afe59c7c64dd0
Andrew Janke <a.janke@gmail.com>
2010-02-13 15:31:13 +0000

    * bumped version

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ The required m4 files were not found.
 You need to check these out from their repository
 using
 
-    cvs -d :ext:shadow.bic.mni.mcgill.ca:/software/source checkout -d m4 libraries/mni-acmacros
+    cvs -d cvs.bic.mni.mcgill.ca:/private-cvsroot checkout -d m4 libraries/mni-acmacros
 
 (yes, two '-d' options)
 Then re-run autogen.sh.
